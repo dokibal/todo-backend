008fb1d039f624b7d1a7cf3dc42db7b815cb2682
dokibal <doktor.balazs1@gmail.com>
2023-09-19 09:05:28 +0200

    Restore Controller dependent CORS config

--- src/main/java/bd/todo/controller/TodoController.java
+++ src/main/java/bd/todo/controller/TodoController.java
@@ -3,19 +3,23 @@ package bd.todo.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bd.todo.model.Todo;
 import bd.todo.service.TodoService;
 
+@CrossOrigin(origins = { "http://localhost:3000", "https://main--amazing-cajeta-f366f2.netlify.app" })
 @RestController
+@RequestMapping("/api/v1/")
 public class TodoController {
 
 	private TodoService todoService;
--- src/main/java/bd/todo/controller/UserController.java
+++ src/main/java/bd/todo/controller/UserController.java
@@ -1,17 +1,21 @@
 package bd.todo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bd.todo.model.User;
 import bd.todo.service.UserService;
 
+@CrossOrigin(origins = { "http://localhost:3000", "https://main--amazing-cajeta-f366f2.netlify.app" })
 @RestController
+@RequestMapping("/api/v1/")
 public class UserController {
 
 	private UserService userService;
